75abe97523d727025a4a91ecd697f1a5497e33a9
Xavier TOLZA <xto@ffly4u.com>
2019-02-04 10:33:11 +0100

    Updated project: requirements, setup and dependencies

--- setup.py
+++ setup.py
@@ -5,13 +5,20 @@ from Cython.Distutils import build_ext
 import numpy
 
 _music = Extension(
-    name = "_music",
-    sources = ["cmusic.c","_music.pyx"],
-    include_dirs = ['blas','m',numpy.get_include()],
-    libraries = ["m","blas"],
-    extra_compile_args = ["-std=c99","-fopenmp"],
-    extra_link_args = ["-fopenmp"])
-                
-setup(ext_modules=[_music],
-      cmdclass = {'build_ext': build_ext})
+    name="_music",
+    sources=["cmusic.c", "_music.pyx"],
+    include_dirs=['blas', 'm', numpy.get_include()],
+    libraries=["m", "blas"],
+    extra_compile_args=["-std=c99", "-fopenmp"],
+    extra_link_args=["-fopenmp"])
 
+setup(
+    name="doamusic",
+    version='1.0',
+    description='DOA finding using MUSIC algorithm in python & cython',
+    author='github.com/yump',
+    author_email='github.com/yump',
+    packages=["doamusic"],
+    ext_modules=[_music],
+    cmdclass={'build_ext': build_ext}
+)
--- util.py
+++ util.py
@@ -20,42 +20,47 @@ import numpy as np
 import scipy as sp
 import math
 
-def aoa2prop(th,ph):
+
+def aoa2prop(th, ph):
     """Propogation vectors from angles of arrival. Result is Nx3."""
-    return np.array((-np.sin(th)*np.cos(ph), #x
-                     -np.sin(th)*np.sin(ph), #y
-                     -np.cos(th)             #z
-                   )).T # we want Nx3 not 3xN
+    return np.array((-np.sin(th) * np.cos(ph),  # x
+                     -np.sin(th) * np.sin(ph),  # y
+                     -np.cos(th)  # z
+                     )).T  # we want Nx3 not 3xN
 
-def aoa2prop_scalar(th,ph):
+
+def aoa2prop_scalar(th, ph):
     """Slightly faster for single arguments, 'cause numpy scalars are slow."""
-    return np.array((-math.sin(th)*math.cos(ph), #x
-                     -math.sin(th)*math.sin(ph), #y
-                     -math.cos(th)               #z
-                   ))
+    return np.array((-math.sin(th) * math.cos(ph),  # x
+                     -math.sin(th) * math.sin(ph),  # y
+                     -math.cos(th)  # z
+                     ))
 
-def awgn(sig,snrdb,sigpower=0):
+
+def awgn(sig, snrdb, sigpower=0):
     """Additive white gaussian noise.  Assumes signal power is 0 dBW"""
     if sp.iscomplexobj(sig):
-        noise = (sp.randn(*sig.shape) + 1j*sp.randn(*sig.shape))/math.sqrt(2)
+        noise = (sp.randn(*sig.shape) + 1j * sp.randn(*sig.shape)) / math.sqrt(2)
     else:
         noise = sp.randn(*sig.shape)
-    noisev = 10**((sigpower - snrdb)/20)
-    return sig + noise*noisev
+    noisev = 10 ** ((sigpower - snrdb) / 20)
+    return sig + noise * noisev
+
 
 def makesamples(antennas, theta, phi, num_samples=1, snr=120):
     """
     Generate sample data for an incident signal from inclination=theta,
     azimuth=phi direction.
     """
-    assert antennas.shape[1] == 3 # no single antennas, must be in R3
+    assert antennas.shape[1] == 3  # no single antennas, must be in R3
 
-    antennas = antennas * (2*sp.pi) # convert from wavelengths to radians
-    propvec = aoa2prop(theta,phi)
-    phases = sp.dot(antennas,propvec)
+    antennas = antennas * (2 * sp.pi)  # convert from wavelengths to radians
+    propvec = aoa2prop(theta, phi)
+    phases = sp.dot(antennas, propvec)
     # add some random phase modulation so signals are uncorrelated
-    phases = phases + sp.randn(num_samples,1)*0.1
-    return sp.exp(1j*phases)
+    phases = phases + sp.randn(num_samples, 1) * 0.1
+    return sp.exp(1j * phases)
+
 
 def eigsort(eigresult):
     """
@@ -63,7 +68,8 @@ def eigsort(eigresult):
     eignevalue magnitude
     """
     ix = sp.argsort(abs(eigresult[0]))
-    return ( eigresult[0][ix], eigresult[1][:,ix] )
+    return (eigresult[0][ix], eigresult[1][:, ix])
+
 
 def sph2cart(sph):
     """
@@ -85,8 +91,9 @@ def sph2cart(sph):
     x = sph[0] * np.sin(sph[1]) * np.cos(sph[2])
     y = sph[0] * np.sin(sph[1]) * np.sin(sph[2])
     z = sph[0] * sp.cos(sph[1])
-    cart = np.squeeze(np.array((x,y,z)).T)
+    cart = np.squeeze(np.array((x, y, z)).T)
     return cart
+
 
 def cart2sph(cart):
     """
@@ -105,19 +112,28 @@ def cart2sph(cart):
         X axis.
     """
     cart = np.atleast_2d(cart).T
-    r = np.sqrt(np.sum(cart**2,axis=0))
-    th = np.arctan2( np.sqrt(np.sum(cart[:2]**2,axis=0)), cart[2] )
-    ph = np.arctan2(cart[1],cart[0])
-    cart = np.squeeze(np.array((r,th,ph)).T)
+    r = np.sqrt(np.sum(cart ** 2, axis=0))
+    th = np.arctan2(np.sqrt(np.sum(cart[:2] ** 2, axis=0)), cart[2])
+    ph = np.arctan2(cart[1], cart[0])
+    cart = np.squeeze(np.array((r, th, ph)).T)
     return cart
 
-def aoa_diff_rad(aoa_a,aoa_b):
+
+def aoa_diff_rad(aoa_a, aoa_b):
     """
     Calculate the difference between two angles of arrival, in radians.
     """
     # Prepend radius 1 and convert to cartesian.
-    cart_a = sph2cart( (1,) + tuple(aoa_a) )
-    cart_b = sph2cart( (1,) + tuple(aoa_b) )
+    cart_a = sph2cart((1,) + tuple(aoa_a))
+    cart_b = sph2cart((1,) + tuple(aoa_b))
     # Property of dot product between vectors.
-    return sp.arccos(sp.dot(cart_a,cart_b))
+    return sp.arccos(sp.dot(cart_a, cart_b))
 
+
+def antenna_array(n_antennas, wavelength, disposition="linear"):
+    if disposition == "linear":
+        x = np.linspace(-wavelength / 4, wavelength / 4, n_antennas)
+        y, z = np.zeros_like(x), np.zeros_like(x)
+    else:
+        raise ValueError("Not supported disposition: %s" % disposition)
+    return np.array([x, y, z]).T
